8f00d40d804f47ffb1a10742f9746f58aee0d3c9
Wassem Darkwa <waseemdarkwa@gmail.com>
2023-01-22 02:28:11 +0000

    Layout component

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,20 @@
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
-
+import Link from 'next/link';
 export default function Home() {
   return (
     //Global container
     <div className=' text-gray-800 flex-1'>
-      <Navbar />
       <h1 className='text-6xl'>HomePage</h1>
-      <p className='text-3xl'>
+      <p className='text-3xl mb-4'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
         At aut soluta ut tempora totam unde impedit ullam
         repellendus a atque.
       </p>
-      <Footer />
+
+      <Link href='/ninjas'>
+        <a className='mb-3'>See all listings</a>
+      </Link>
     </div>
   );
 }
--- pages/ninjas/test.js
+++
@@ -1,9 +0,0 @@
-const test = () => {
-  return (
-    <div>
-      <h1 className='text-6xl'>Test charle !</h1>
-    </div>
-  );
-};
-
-export default test;
