e32c6e584e0a920659bb65da871e086e68ef7db8
Wassem Darkwa <waseemdarkwa@gmail.com>
2023-01-23 14:11:05 +0000

    get Static props

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,7 +1,28 @@
-const allninjas = () => {
+export const getStaticProps = async () => {
+  const res = await fetch(
+    'https://jsonplaceholder.typicode.com/users'
+  );
+
+  const data = await res.json();
+
+  return {
+    props: {
+      ninjas: data,
+    },
+  };
+};
+
+const allninjas = ({ ninjas }) => {
   return (
     <div>
       <h1 className='text-6xl'>All Ninjas</h1>
+      {ninjas.map((ninja) => (
+        <div key={ninja.id}>
+          <a>
+            <h3 className='font-bold text-lg'> {ninja.name}</h3>
+          </a>
+        </div>
+      ))}
     </div>
   );
 };
